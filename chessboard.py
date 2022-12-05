fa4d047102bf0a1ddf29cc8ee8cd18ca271d1aa2
onkar-virdi <116166809+onkar-virdi@users.noreply.github.com>
2022-12-05 14:08:01 +1300

    Add files via upload

---
+++ chessboard.py
@@ -0,0 +1,42 @@
+import turtle
+import time
+t = turtle.Turtle()
+#time.sleep(2)
+def box(ln):
+     for i in range(4):
+       t.forward(ln)
+       t.left(90)
+t.speed(0)
+x = 0
+y = 0
+count = 2
+
+while True:
+    t.goto(x,y)
+    t.pendown()
+
+    x += 20
+    t.begin_fill()
+    if count % 2 ==0:
+        t.fillcolor("white")
+    else:
+        t.fillcolor("black")
+
+    count +=1
+
+    box(20)
+    t.end_fill()
+    if x >= 20*8:
+        x = 0
+        y += 20
+        t.penup()
+        count +=1
+
+    if y >= 20*8:
+        break
+
+
+t.hideturtle()
+
+
+turtle.done()
